47b79e423c420c96f7ee6f4d68a5623542fb4625
Zach 'Leif' Eriksen <zeriksen@clientresourcesinc.com>
2018-08-10 13:07:04 -0500

    Initial Commit

---
+++ FNOGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FNOGame
+//
+//  Created by Zach Eriksen on 8/10/18.
+//  Copyright © 2018 cri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
